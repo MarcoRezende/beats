9ccfeb0de9ca80400b7f6a54a02a4f435b3a40b5
Marco <marcorezende@outlook.com.br>
2019-10-10 18:37:52 -0300

    criando e organizando estrutura inicial + component "home"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFullpageModule } from '@fullpage/angular-fullpage';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularFullpageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
