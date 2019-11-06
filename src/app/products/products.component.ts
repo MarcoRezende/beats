629b38ba04189d9abc75b8a6e65687d0cde0c5d8
Marco <marcorezende@outlook.com.br>
2019-11-06 17:54:40 -0300

    reduzindo logica para obter o produto com base no queryParams

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -9,45 +9,48 @@ export default sampleData;
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
-  
+
   home: boolean = false;
   data: any = sampleData;
   catalog: string = "";
-  product: any = "";
+  query: any = [
+    {name:'vst-expansion',prod:'vst'}, 
+    {name:'drum-kit',prod:'drum'}, 
+    {name:'loops',prod:'loops'}, 
+    {name:'midi-kit',prod:'midi'}
+  ];
+  product: any;
 
-  constructor(private route: ActivatedRoute) { 
-    this.catalog = this.route.queryParams._value.catalog
-    if (this.catalog === 'vst-expansion') {
-      this.product = this.data.products.vst;
-    } else if (this.catalog === 'drum-kit') {
-      this.product = this.data.products.drum
-    } else if (this.catalog === 'loops') {
-      this.product = this.data.products.loops
-    } else if (this.catalog === 'midi-kit') {
-      this.product = this.data.products.midi
-    } else {
-      // redirecionar para pagina de erro 404
+  constructor(private route: ActivatedRoute) {
+    if (this.route.queryParams._value.catalog) { 
+      this.route.queryParams.subscribe(values => {
+        this.catalog = values.catalog
+      });
+      for (let i = 0; i < this.query.length; i++) {
+        if (this.catalog === this.query[i].name) {
+          this.product = this.data.products[this.query[i].prod];
+        } else {
+          // redirecionar para 404
+        }
+      }
     }
   }
 
 
   ngOnInit() {
-   	this.home = true;
-
-   	var myToggle = function(element, class0, class1) {
+    var myToggle = function(element, class0, class1) {
       if ( !element.classList.contains(class1) ) {
       	element.classList.remove(class0);
       	element.classList.add(class1);
-   	  }
+      }
     }
 
-   	if (this.home) {
-	   	let header = document.getElementsByTagName('header')[0];
-	   	let nav = document.getElementsByTagName('nav')[0];
-	   	myToggle(header, 'header-home', 'header-not-home');
-	   	myToggle(nav, 'home-nav', 'not-home-nav');
-	   	console.log(nav)	
-   	}
-  }
+    if (!this.home) {
+     let header = document.getElementsByTagName('header')[0];
+     let nav = document.getElementsByTagName('nav')[0];
+     myToggle(header, 'header-home', 'header-not-home');
+     myToggle(nav, 'home-nav', 'not-home-nav');
+   }
+ }
 
 }
