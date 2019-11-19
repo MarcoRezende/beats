12e80a299f0b7323e705495d6bb793a232c13841
Marco <marcorezende@outlook.com.br>
2019-11-19 19:40:18 -0300

    adicionando tarefa no gulp para comprimir e redimensionar imagens

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,44 @@
 
 'use strict';
  
-var gulp = require('gulp');
-var sass = require('gulp-sass');
- 
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
+const imageResize = require('gulp-image-resize');
+const changed = require("gulp-changed");
+const rename = require("gulp-rename"); 
+
 sass.compiler = require('node-sass');
  
 gulp.task('sass', function () {
   return gulp.src('./src/sass/styles.scss')
     .pipe(sass.sync().on('error', sass.logError))
     .pipe(gulp.dest('./src'));
+});
+
+gulp.task("minifyImgs", function () {
+  gulp.src("./src/assets/imgs/*.{jpg,png}")
+  	.pipe(changed("./src/assets/minified"))
+  	.pipe(imagemin())
+    .pipe(rename(function (path) { path.basename += "-compressed"; }))
+    .pipe(gulp.dest("./src/assets/imgs/"));
+});
+
+gulp.task("minifyProductImg", function () {
+  gulp.src("./src/assets/imgs/products/*.{jpg,png}")
+  	.pipe(changed("./src/assets/imgs/products"))
+  	.pipe(imagemin())
+    .pipe(gulp.dest("./src/assets/imgs/products"))
+  gulp.series('resizeProductImg');
+});
+
+gulp.task("resizeProductImg", function () {
+  gulp.src("./src/assets/imgs/products/*.{jpg,png}")
+  	// .pipe(changed("./src/assets/imgs/products"))
+    .pipe(imageResize({ width: 600, height : 401, crop : true }))
+    .pipe(rename(function (path) { path.basename += "-600x401"; }))
+  	.pipe(imagemin())
+    .pipe(gulp.dest("./src/assets/imgs/products"));
 });
  
 gulp.task('watch', function () {
