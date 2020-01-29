92382cbd58480e8922c75ecbdd604b59b1bf4de3
Marco <marcorezende@outlook.com.br>
2020-01-29 16:48:50 -0300

    removendo footer

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -83,23 +83,17 @@ export class ProductsComponent implements OnInit {
   goToTop(ele) {
     if (ele[0] !== "activeLink") {
       this.allImgs = []; 
-
-      // removendo classe 'hidden-footer'
-      let footer = document.querySelector('footer')
-      footer.classList.remove('hidden-footer');
       
       this.imgsLoaded = false;
       // scroll to the top of the page
       document.getElementById('top').scrollIntoView(true);
-    }
+    } 
   }
 
   updateImgState(item) {
     this.allImgs.push(item)
-    let footer = document.querySelector('footer')
 
     if (this.allImgs.length === this.pagedItems.length) {
-      footer.classList.add('hidden-footer');
       this.imgsLoaded = true;
     }
   }
